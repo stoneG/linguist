91d633e5389a63ccb3f4b4a315c76fbc7facde7f
stoneG <me@sitong>
2012-12-12 17:31:21 -0500

    adding in psycopg2 and postgres components

--- build_frequencies.py
+++ build_frequencies.py
@@ -3,6 +3,10 @@ import string
 import time
 import os
 
+import psycopg2
+
+# Database: linguist
+
 # Construct OWL dictionary
 class OfficialWords(object):
     def __init__(self, OWLpath):
@@ -45,8 +49,6 @@ class TextCorpus(object):
 
 
 class Text(object):
-
-
     def __init__(self, file_name):
         f = open(file_name, 'r')
         self.string = f.read()
@@ -58,19 +60,46 @@ class Text(object):
         match = re.findall(self.word_pat, self.string)
         return match
 
+class Database(object):
+    def __init__(self, dbName, dbUser, wordCount):
+        """Param : Description
+        dbName   : Str name of the database to connect to
+        dbUser   : Str name of the psql user
+        wordCount: Dict of word counts
+        """
+        self.name = dbName
+        self.user = dbUser
+        self.word_count = wordCount
 
-def current_time():
-    return time.time()
+    def create_cursor(self):
+        name_and_user = "dbname={0} user={1}".format(self.name, self.user)
+        self.conn = psycopg2.connection(name_and_user)
+        self.cur = self.conn.cursor()
+
+    def commit_and_close(self):
+        self.conn.commit()
+        self.cur.close()
+        self.conn.close()
+
+    def create_table(self):
+        """Create WordCount table in database."""
+        name = 'WordCount'
+        self.create_cursor()
+        sql = 'CREATE TABLE {0} (word varchar PRIMARY KEY, count integer);'.format(name)
+        self.cur.execute(sql)
+        for word, count in self.word_count.items():
+            sql = 'INSERT INTO {0} (word, count) VALUES (%s, %s);'.format(name)
+            self.cur.execute(sql, (word, count))
+        self.commit_and_close()
+
 
 OWLpath = 'wordlist/TWL06.txt'
 textFolder = 'abc'
 dic = {}
 def main():
     global dic
-    t = current_time()
     text_corpus = TextCorpus(textFolder, OWLpath)
     dic = text_corpus.build()
-    print 'Program took %d seconds' % (current_time() - t)
 
 if __name__ == '__main__':
     main()
