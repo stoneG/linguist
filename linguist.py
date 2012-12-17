9441d12c94af971031b6e4b9ab78e5945b5f73aa
stoneG <me@sitong>
2012-12-17 18:30:35 -0500

    added routing for logging in and logging out

--- linguist.py
+++ linguist.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash, session, redirect, url_for
+import pdb
 
 from db import *
 
@@ -15,7 +16,9 @@ Users = UserTable('linguist', 'sitong', tableName='Users')
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    if 'username' in session:
+        return render_template('main.html', username=session['username'])
+    return render_template('main.html', username=False)
 
 @app.route('/add', methods=['POST'])
 def rarity():
@@ -29,8 +32,13 @@ def rarity():
 
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
+    if session['logged_in']:
+        return render_template('login.html', logged_in=True)
     error = ''
-    if request.method == 'POST' and request.form['register']:
+    registering = False
+    if 'register' in request.method:
+        registering = True
+    if request.method == 'POST' and registering:
         error = Users.register(request.form['username'], request.form['password'])
         if not error:
             session['logged_in'] = True
@@ -38,9 +46,15 @@ def login():
     elif request.method == 'POST':
         error = Users.login(request.form['username'], request.form['password'])
         if not error:
+            session['username'] = request.form['username']
             session['logged_in'] = True
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('main'))
 
 #if __name__ == '__main__':
 #    print 'Running Linguist on localhost:5000'
